d13ad1a69fdf3ad12639e66bd87c30f188b22e84
Pierre Wadoux <pierre.wadoux7@gmail.com>
2023-05-02 12:55:24 -0400

    updated simple_serial to include non bloc input

--- scripts/simple_serial.py
+++ scripts/simple_serial.py
@@ -1,11 +1,18 @@
 import serial
-ser = serial.Serial("/dev/ttyS0", 115200)
-cmd = "G28"
-ser.write(cmd.encode())
-user = ""
-while "stop" not in user:
-	user = input()
+import threading
+
+def send_command():
+	user = input("chitu g code to send")
 	ser.write(user.encode())
+
+ser = serial.Serial("/dev/ttyS0", 115200)
+ser.write("M156 S10".encode())
+
+x = threading.Thread(target=send_command)
+
+user = ""
+x.start()
+while "stop" not in user:
 	try:
 		res = ser.readline().decode()
 		print("Saturn :" , res)
