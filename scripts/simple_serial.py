99da92f96f6a092bdcbead7e735ec0179db7ea0e
Pierre Wadoux <pierre.wadoux7@gmail.com>
2023-05-02 13:04:21 -0400

    bug fix

--- scripts/simple_serial.py
+++ scripts/simple_serial.py
@@ -1,23 +1,26 @@
 import serial
 import threading
 
+
 def send_command():
-	while "stop" not in user:
-		user = input("chitu g code to send")
-		ser.write(user.encode())
+    user = ""
+    while "stop" not in user:
+
+        user = input("chitu g code to send")
+        ser.write(user.encode())
+
 
 def serial_read(ser: serial.Serial):
-	while True:
-		msg = ser.readline().decode()
-		print(msg)
+    while True:
+        ser.write("M156 S10".encode())
+        msg = ser.readline().decode()
+        print(msg)
+
 
 ser = serial.Serial("/dev/ttyS0", 115200)
-ser.write("M156 S10".encode())
 
 
 x = threading.Thread(target=send_command)
 x.start()
 y = threading.Thread(target=send_command, args=ser)
 y.start()
-
-
