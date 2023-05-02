12dafbdcfbac1db7a6715f498826f5192439ebc2
Pierre Wadoux <pierre.wadoux7@gmail.com>
2023-05-02 13:11:56 -0400

    idem

--- scripts/simple_serial.py
+++ scripts/simple_serial.py
@@ -12,9 +12,10 @@ def send_command():
 
 def serial_read(ser: serial.Serial):
     while True:
-        ser.write("M156 S10".encode())
-        msg = ser.readline().decode()
-        print(msg)
+        if ser.in_waiting > 0:
+            ser.write("M156 S10".encode())
+            msg = ser.readline().decode()
+            print(msg)
 
 
 ser = serial.Serial("/dev/ttyS0", 115200)
@@ -22,5 +23,5 @@ ser = serial.Serial("/dev/ttyS0", 115200)
 
 y = threading.Thread(target=serial_read, args=ser)
 y.start()
-x = threading.Thread(target=send_command)
-x.start()
+# x = threading.Thread(target=send_command)
+# x.start()
