4c621622f5df016015db1c1b3dd40104a9e08e72
Pierre Wadoux <pierre.wadoux7@gmail.com>
2023-05-02 13:02:29 -0400

    two thread

--- scripts/simple_serial.py
+++ scripts/simple_serial.py
@@ -2,20 +2,22 @@ import serial
 import threading
 
 def send_command():
-	user = input("chitu g code to send")
-	ser.write(user.encode())
+	while "stop" not in user:
+		user = input("chitu g code to send")
+		ser.write(user.encode())
+
+def serial_read(ser: serial.Serial):
+	while True:
+		msg = ser.readline().decode()
+		print(msg)
 
 ser = serial.Serial("/dev/ttyS0", 115200)
 ser.write("M156 S10".encode())
 
-x = threading.Thread(target=send_command)
 
-user = ""
+x = threading.Thread(target=send_command)
 x.start()
-while "stop" not in user:
-	try:
-		res = ser.readline().decode()
-		print("Saturn :" , res)
-	except Exception as e:
-		print(e)
-ser.close()
+y = threading.Thread(target=send_command, args=ser)
+y.start()
+
+
